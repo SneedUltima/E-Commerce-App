3cde0cade5157eb605503d1dc5d1e325591b6e94
Dylan <dylan.cobham@yahoo.com.au>
2023-05-19 20:11:53 +0930

    build: add categories component

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Slider from "../../Components/Slider";
 import FeaturedProjects from "../../Components/FeaturedProjects";
+import Categories from "../../Components/Categories";
 
 const Home = () => {
   return (
     <div>
       <Slider />
       <FeaturedProjects type="Featured" />
+      <Categories />
       <FeaturedProjects type="Trending" />
     </div>
   );
